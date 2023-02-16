050c272009a536ebf38ff8f5a1f52134762902ef
SOO JEONG KIM <poohlicious0@gmail.com>
2023-02-16 17:22:49 +0900

    knowledge graph node color size done

--- chatroom/views.py
+++ chatroom/views.py
@@ -23,6 +23,7 @@ import os
 import sys
 import networkx as nx
 import matplotlib.pyplot as plt
+from networkx.drawing.nx_pydot import graphviz_layout
 #flask로 웹페이지 간단하게 만들고, mongodb, postgresql, 증상 웹페이지에 인풋 -> 의심되는 병 진단 결과 아웃풋,,
 #서버에 bert api로 인풋을 get전송,,  flask 웹프레임워크,, 3090에서 다처리하고 처리한걸 flask로 보내기. (이걸 django로 바꾸기)
 
@@ -94,11 +95,50 @@ def room(request, lm_name):
     return render(request, 'chatroom/room.html', context)
 
 def create_graph(keyword):
+    '''
+    plt.figure(figsize=(16, 8))
+    plt.margins(x=0.1, y=0.02)
+    example = {'연금시장': {'채권시장': 0.4153662323951721, '시장': 0.41456204652786255, '주식시장': 0.3486636281013489,
+                        '경제': 0.25629764795303345, '단계': 0.24219422042369843, '규모': 0.24082091450691223,
+                        '거래': 0.22897380590438843, '국내': 0.2052801549434662, '주선': 0.1902499794960022,
+                        '리스': 0.1724136471748352},
+               '경제': {'주선': 0.2807018756866455, '채권시장': 0.2597755193710327, '하락': 0.2146831750869751,
+                      '산업': 0.21218357980251312, '경쟁': 0.2039693295955658, '한국': 0.20326876640319824,
+                      '단계': 0.20303569734096527, '리스': 0.1892050802707672, '수익': 0.17960302531719208,
+                      '최근': 0.17808492481708527},
+               '채권': {'측정': 0.3019123673439026, '특성': 0.27175581455230713, '채권시장': 0.2508116364479065,
+                      '우수': 0.23209211230278015, '수준': 0.22973835468292236, '관련': 0.22968417406082153,
+                      '조원': 0.21655943989753723, '금액': 0.20694774389266968, '인력': 0.19489142298698425,
+                      '주식시장': 0.18427613377571106}}
+    G = nx.DiGraph(example)
+    #G = dict(DG.degree)
+    # 아래 layout 중에서 마음에 드는 것을 쓰면 됩니다.
+    pos = nx.spring_layout(G)
+    #pos = graphviz_layout(G, prog='twopi')
+    #pos = nx.circular_layout(G)
 
+    nx.draw_networkx_nodes(
+        G, pos, node_color=[n[1]['weight'] for n in G.nodes(data=True)], node_shape='h',
+        node_size=3000, cmap=plt.cm.Blues, alpha=0.9
+    )
+    nx.draw_networkx_edges(
+        G, pos, edge_color=[e[2]['weight'] for e in G.edges(data=True)],
+        width=5, edge_cmap=plt.cm.Greys, style='dashed'
+    )
+    nx.draw_networkx_labels(
+        G, pos, font_family='sans-serif', font_color='black', font_size=10, font_weight='bold'
+    )
+    plt.axis('off')
+    # plt.show()
+    plt.savefig("D:/2022_2_GraphTransformer\graphtransformer\diya-chat\diyaChat\chatroom\static\chatroom\img\graph.png")
+
+    '''
     font_name = fm.FontProperties(fname="C:/Users\poohl/anaconda3\Lib\site-packages\matplotlib\mpl-data/fonts/ttf/NanumGothic.ttf").get_name()
     rc('font', family=font_name)
     plt.title('Knowledge Graph for Keyword')
     #print(keyword)#debug
+    plt.figure(figsize=(16,16))
+    plt.margins(x=0.1, y=0.02)
 
     example = {'연금시장': {'채권시장': 0.4153662323951721, '시장': 0.41456204652786255, '주식시장': 0.3486636281013489, '경제': 0.25629764795303345, '단계': 0.24219422042369843, '규모': 0.24082091450691223, '거래': 0.22897380590438843, '국내': 0.2052801549434662, '주선': 0.1902499794960022, '리스': 0.1724136471748352},
         '경제': {'주선': 0.2807018756866455, '채권시장': 0.2597755193710327, '하락': 0.2146831750869751, '산업': 0.21218357980251312, '경쟁': 0.2039693295955658, '한국': 0.20326876640319824, '단계': 0.20303569734096527, '리스': 0.1892050802707672, '수익': 0.17960302531719208, '최근': 0.17808492481708527},
@@ -106,17 +146,39 @@ def create_graph(keyword):
     DG = nx.DiGraph(example)
     d = dict(DG.degree)
     #connect = nx.connected_components(DG.subgraph(d.keys()))
-    subgraph = DG.subgraph(d)#list(connect)
-    color_map = []
-    for i, sub in enumerate(subgraph):
-        sub_color = rd.random()
-        for j in range(len(sub)):
-            color_map.append(sub_color)
-    pos = nx.shell_layout(DG)
-    nx.draw_shell(DG)
-    nx.draw(DG, pos, font_family=font_name, font_size=15, with_labels=True, node_size=[v * 200 for v in d.values()]),
+
+    G = nx.Graph()
+    G_weight = [(a, b) for a, b in d.items()]
+    G.add_nodes_from([a for a, b in d.items()])
+   # G.add_edges_from(        (e[0], e[1]) for e in        DG.edges(data=True)    )
+    #G = nx.DiGraph(example)
+    # G = dict(DG.degree)
+    # 아래 layout 중에서 마음에 드는 것을 쓰면 됩니다.
+    pos = nx.spring_layout(G)
+    # pos = graphviz_layout(G, prog='twopi')
+    #pos = nx.circular_layout(G)
+
+    nx.draw_networkx_nodes(
+        G, pos, node_color=[n[1] for n in G_weight], node_shape='h',
+        node_size=3000, cmap=plt.cm.Blues#, alpha=0.9
+    )
+
+    nx.draw_networkx_edges(
+        DG, pos, edge_color='black',
+        width=1, edge_cmap=plt.cm.Greys, style='dashed'
+    )
+
+    nx.draw_networkx_labels(
+        G, pos, font_family=font_name, font_color='black', font_size=10, font_weight='bold'
+    )
+    plt.axis('off')
+
+    #pos = nx.shell_layout(G)
+    #nx.draw_shell(G)
+    #nx.draw(G, pos, font_family=font_name, font_size=15, with_labels=True, node_size=[v for v in d.values()]),
        # node_color=color_map)#_networkx_labels
     plt.savefig("D:/2022_2_GraphTransformer\graphtransformer\diya-chat\diyaChat\chatroom\static\chatroom\img\graph.png")
+
 
 def rag_model(request):#TODO
     query = request.args.get("query")
