08c9331ffb15dae2e37355a0f590f9bd1b600a5f
SOO JEONG KIM <poohlicious0@gmail.com>
2023-02-22 23:56:44 +0900

    Delete tf_model directory

--- tf_model/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TfModelConfig(AppConfig):
-    name = 'tf_model'
--- tf_model/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import random
-import tensorflow as tf
-# Create your views here.
